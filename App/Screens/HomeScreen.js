e2d319e283da884481c38a2235c50b6b2da216e6
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-07-20 18:46:47 +0530

    added category gql query

--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,7 +1,12 @@
 import React, { memo } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+
+import { CATEGORIES } from "../Apollo/CategoryQuery";
 
 const HomeScreen = () => {
+  const { loading, error, data } = useQuery(CATEGORIES);
+  // console.log("data", data);
   return (
     <View>
       <Text>HomeScreen</Text>
